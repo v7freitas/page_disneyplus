d4e957b7d9669356e4fd14bdedb8643932fdf6e5
Vinicius Freitas <vff2804@gmail.com>
2024-10-16 22:42:30 -0300

    add faq and footer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("[data-tab-button]");
 
+  const questions = document.querySelectorAll("[data-faq-question]");
+
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (button) {
       const tabTarget = button.target.dataset.tabButton;
@@ -11,7 +13,18 @@ document.addEventListener("DOMContentLoaded", function () {
       button.target.classList.add("shows__tabs__button--is--active");
     });
   }
+
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", openOrCloseAnswer);
+  }
 });
+
+function openOrCloseAnswer(element) {
+  const classType = "faq__questions__item--is--open";
+  const fatherElement = element.target.parentNode;
+
+  fatherElement.classList.toggle(classType);
+}
 
 function removeActiveButton() {
   const buttons = document.querySelectorAll("[data-tab-button]");
